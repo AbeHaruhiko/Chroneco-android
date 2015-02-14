4461d4327d01792faa34ea6a1cf3bfee0dad8296
AbeHaruhiko <caliconography@gmail.com>
2015-02-14 21:28:09 +0900

    不要クラス削除など。

--- app/src/main/java/jp/caliconography/welco/activity/MemberListActivity.java
+++ app/src/main/java/jp/caliconography/welco/activity/MemberListActivity.java
@@ -1,6 +1,5 @@
 package jp.caliconography.welco.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
@@ -8,51 +7,13 @@ import android.view.MenuItem;
 
 import jp.caliconography.welco.R;
 
-/**
- * An activity representing a list of Members. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link MemberDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- * <p/>
- * The activity makes heavy use of fragments. The list of items is a
- * {@link MemberListFragment} and the item details
- * (if present) is a {@link MemberDetailFragment}.
- * <p/>
- * This activity also implements the required
- * {@link MemberListFragment.Callbacks} interface
- * to listen for item selections.
- */
 public class MemberListActivity extends FragmentActivity
         implements MemberListFragment.Callbacks {
-
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_list);
-
-        if (findViewById(R.id.member_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
-            ((MemberListFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.member_list))
-                    .setActivateOnItemClick(true);
-        }
-
-        // TODO: If exposing deep links into your app, handle intents here.
     }
 
     @Override
@@ -78,24 +39,5 @@ public class MemberListActivity extends FragmentActivity
      */
     @Override
     public void onItemSelected(String id) {
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(MemberDetailFragment.ARG_ITEM_ID, id);
-            MemberDetailFragment fragment = new MemberDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.member_detail_container, fragment)
-                    .commit();
-
-        } else {
-            // In single-pane mode, simply start the detail activity
-            // for the selected item ID.
-            Intent detailIntent = new Intent(this, MemberDetailActivity.class);
-            detailIntent.putExtra(MemberDetailFragment.ARG_ITEM_ID, id);
-            startActivity(detailIntent);
-        }
     }
 }
